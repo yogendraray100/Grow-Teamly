c29394201b6f6a162db2f92389d6e76521cff509
yogendraray100 <87217256+yogendraray100@users.noreply.github.com>
2022-06-24 16:24:11 +0545

    add existing files

--- lib/features/dashboard/views/dashboard_view.dart
+++ lib/features/dashboard/views/dashboard_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testapp/app/routes.dart';
+import 'package:testapp/features/dashboard/model/dashboard_card_model.dart';
+import 'package:testapp/features/dashboard/services/dashboard_services.dart';
 import 'package:testapp/helpers/confirmation_dialog.dart';
 import 'package:testapp/home.dart';
 import 'package:testapp/injector.dart';
@@ -12,11 +14,31 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
+  DashboardCardModel? dashboardCard;
+  bool _isloading = true;
+  @override
+  void initState(){
+    super.initState();
+    fetchDashboardCardData();
+  }
+  fetchDashboardCardData() async {
+    DashboardCardModel? response = 
+    await DashboardServices().fetchDashboardCardData();
+    dashboardCard = response;
+    _isloading = false;
+    setState(() {
+      
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard"),
+        title: Text("DashboardView",style: TextStyle(fontSize: 20,color: Colors.red),),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
         actions: [
 
            IconButton(onPressed: () async{
@@ -54,6 +76,30 @@ class _DashboardViewState extends State<DashboardView> {
           icon: Icon(Icons.logout)),
          ],
       ),
+      body: _isloading? Center(
+        child: CircularProgressIndicator(color: Colors.redAccent,),
+      ):Column(
+        children: [
+          Padding(padding: EdgeInsets.only(left: 20),
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10.0),
+                height: 50,
+                width: size.width * 0.5,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(color: Colors.blue,
+                borderRadius: BorderRadius.circular(15),),
+                child: Text(dashboardCard!.myMissingCheckoutCount.toString(),style: TextStyle(fontSize: 20,color: Colors.redAccent),),
+              
+              ),
+              SizedBox(width: 50,),
+              Text(dashboardCard!.myLeaveBalance.toString(),style: TextStyle(fontSize: 20,color: Colors.redAccent),),
+            ],
+          ),
+          )
+        ],
+      )
       
     );
   }
