27525f7988ce8ec7e04d87284cde181b6c8d85a8
yogendraray100 <87217256+yogendraray100@users.noreply.github.com>
2022-07-08 08:51:39 +0545

    expanded,spacer,cached netwrok image

--- lib/features/leaves/views/leaves_view.dart
+++ lib/features/leaves/views/leaves_view.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:testapp/constants/image_constants.dart';
  class LeavesView extends StatefulWidget {
    const LeavesView({ Key? key }) : super(key: key);
  
@@ -11,9 +13,44 @@ import 'package:flutter/material.dart';
    Widget build(BuildContext context) {
      return Scaffold(
        appBar: AppBar(
-         title: Text("Leaves"),
+        automaticallyImplyLeading: false,
+         title:const Text("Leaves"),
        ),
-    
+    body : Column(
+      children : [
+        Container(
+          height: 70,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(children: [
+            Expanded(child: TextFormField(
+              decoration: InputDecoration(contentPadding: EdgeInsets.all(5),hintText: "Search",labelText: "New",fillColor: Colors.white,filled: true),
+            )),
+            IconButton(onPressed: (){}, icon: Icon(Icons.search))
+          ]),
+        ),
+        Expanded(
+          flex: 2,
+          child: CachedNetworkImage(imageUrl: ImageConstants.bcc, height: 120,width: 120,fit: BoxFit.cover,
+          progressIndicatorBuilder: (context, url, progress)=> const Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error)=> const Icon(Icons.error),),
+        ),
+        Expanded(
+          flex: 2,child: Container(
+          color: Colors.blue,
+        )),
+        
+        Expanded(
+          flex: 1,
+          child: const Text("Hello")),
+
+      ]
+    )
      );
    }
  }
